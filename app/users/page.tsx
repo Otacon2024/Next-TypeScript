15112b0a5948b2b637a8072009f0d98756e2cab8
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-17 15:32:56 +0100

    Create page.tsx into the new directory "app > users"

---
+++ app/users/page.tsx
@@ -0,0 +1,10 @@
+//rafce (strands for react arrow function component export)
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
