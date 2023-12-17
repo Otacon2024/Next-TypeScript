c275e66e19bffb5dc9e309e2332c4bb4de50d069
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-17 16:02:56 +0100

    Add Link to "/users" into "add > page.tsx"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1>Hello World</h1></main>
+    <main>
+      <h1>Hello World</h1>
+      {/* We use Link instead of <a> to navigate between pages without reloading the page */}
+      <Link href="/users">Users</Link>
+    </main>
   )
 }
