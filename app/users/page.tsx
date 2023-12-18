b085a36277fc5e2f9f0628aec1286bfc0b04e58a
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-18 13:06:41 +0100

    turn UserPage into an async function component + fetch data from jsonplaceholder.typicode.com website

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,10 @@
 //rafce (strands for react arrow function component export)
 import React from 'react'
 
-const UsersPage = () => {
+const UsersPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await res.json();
+
   return (
     <div>UsersPage</div>
   )
