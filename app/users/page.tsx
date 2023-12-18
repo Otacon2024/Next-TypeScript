c3620d9c70685f354fafea73d9683a3ab4f2d3a0
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-18 13:29:37 +0100

    Add interface User + display users as <li>

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,27 @@
 //rafce (strands for react arrow function component export)
 import React from 'react'
 
+interface User {
+    id: number;
+    name: string;
+}
+
 const UsersPage = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await res.json();
+    const users: User[] = await res.json();
 
   return (
-    <div>UsersPage</div>
+    <>
+        <h1>Users</h1>
+
+        <ul>
+            {users.map(user => 
+                <li key={user.id}>
+                    {user.name}
+                </li>)
+            }
+        </ul>
+    </>
   )
 }
 
