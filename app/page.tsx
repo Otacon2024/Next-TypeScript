b843e15f1d4391b29dda8b121bce4c804450e4ab
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-21 12:00:32 +0100

    Import ProductCard from './components/ProductCard' into 'app > page.tsx'

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
-      {/* We use Link instead of <a> to navigate between pages without reloading the page */}
+      <ProductCard />
       <Link href="/users">Users</Link>
     </main>
   )
