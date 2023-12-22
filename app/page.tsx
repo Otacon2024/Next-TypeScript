cbfbc2072a235ae516926a4baab0a4fe72a5e277
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-22 18:22:34 +0100

    Minor change to app > page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
-      <ProductCard />
       <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   )
 }
