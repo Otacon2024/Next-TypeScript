ef446170fd428c06621593a449c11177a172c64c
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-23 01:23:04 +0100

    Re-add <div> around <AddToCart /> into ProductCard.tsx

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,9 +4,9 @@ import AddToCart from './AddToCart';
 
 const ProductCard  = () => {
   return (
-    // <div className='p-5 my5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+    <div>
       <AddToCart />
-    // </div>
+    </div>
   )
 }
 
