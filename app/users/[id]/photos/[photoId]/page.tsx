445a5dcd03196ab3c8749b411beaea29b2dca9fe
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-29 00:11:11 +0100

    Add PhotoPage component with interface Props { params: { id: number; photoId: number }; } to "app > users > [id] > photos > [photoId] > page.tsx"

---
+++ app/users/[id]/photos/[photoId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  params: { id: number; photoId: number };
+}
+
+const PhotoPage = () => {
+  return <div>PhotoPage</div>;
+};
+
+export default PhotoPage;
