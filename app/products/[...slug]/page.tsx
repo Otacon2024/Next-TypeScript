b51080acd6af34190b1321cf9af965179e388295
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-29 14:38:08 +0100

    Add ProductPage component in "app > products > [...slug] > page.tsx"
    + Add interface Props { params: { slug: string[] }; }
    
    + Pass props (props: Props) to ProductPage component

---
+++ app/products/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  params: { slug: string[] };
+}
+
+const ProductPage = (props: Props) => {
+  return <div>ProductPage</div>;
+};
+
+export default ProductPage;
