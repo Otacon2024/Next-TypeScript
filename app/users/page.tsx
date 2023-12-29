c4ba71c3a54799fd04deb155beb25d387f31be45
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-29 16:54:14 +0100

    Pass ({ searchParams }: Props) argument to UserPage

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,7 +5,7 @@ interface Props {
   searchParams: { sortOrder: string };
 }
 
-const UsersPage = async () => {
+const UsersPage = async ({ searchParams }: Props) => {
   return (
     <>
       <h1>Users</h1>
