c5f8de524696942641e7079f815cc4fa1f022b62
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-30 21:33:53 +0100

    Add "use client" to "app>users>new>page.tsx"
    + Add click event to the <button> "Create"

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,12 @@
+"use client";
 import React from "react";
 
 const NewUserPage = () => {
-  return <button className="btn btn-primary">Create</button>;
+  return (
+    <button className="btn btn-primary" onClick={() => console.log("click")}>
+      Create
+    </button>
+  );
 };
 
 export default NewUserPage;
