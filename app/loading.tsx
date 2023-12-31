aefadc4aeab94829fbbd87117633e2ee7ee71bcf
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2023-12-31 19:45:56 +0100

    Use loading-spinner animation (from daisyui) instead of "Loading..." <p>

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const loading = () => {
-  return <div>Loading...</div>;
+  return <span className="loading loading-spinner loading-md"></span>;
 };
 
 export default loading;
