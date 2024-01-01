c3690c529508bbc6570ccd48eee9c248ca6a8089
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-01 18:14:36 +0100

    Add reset to interface Props {error: Error; reset: () => void;}
    // Next.js use "reset" to pass the reset function to our component automatically.
    
    + Pass ({ error, reset }: Props) to ErrorPage component
    
    + Add <button> with onClick={() => reset()}
    
    // We need the 'use client'; to handle this click event

--- app/error.tsx
+++ app/error.tsx
@@ -3,13 +3,21 @@ import React from "react";
 
 interface Props {
   error: Error;
+  reset: () => void;
 }
 
 // In a real app, you'd probably want to log the error to an error reporting service (like sentri.io).
-const ErrorPage = ({ error }: Props) => {
+const ErrorPage = ({ error, reset }: Props) => {
   console.log("Error", error);
 
-  return <div>An unexpected error occurred.</div>;
+  return (
+    <>
+      <div>An unexpected error occurred.</div>
+      <button className="btn" onClick={() => reset()}>
+        Retry
+      </button>
+    </>
+  );
 };
 
 export default ErrorPage;
