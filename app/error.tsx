66e2f2e9ff4ebc672742f231b584e25dbe8929f5
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-01 17:36:04 +0100

    Add a "tip" comment to ErrorPage component

--- app/error.tsx
+++ app/error.tsx
@@ -5,6 +5,7 @@ interface Props {
   error: Error;
 }
 
+// In a real app, you'd probably want to log the error to an error reporting service.
 const ErrorPage = ({ error }: Props) => {
   return <div>An unexpected error occurred.</div>;
 };
