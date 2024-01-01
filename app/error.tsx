9094b44adf4bc0837abd4fbfca82903677d0117e
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-01-01 17:40:32 +0100

    Log the error in ErrorPage component

--- app/error.tsx
+++ app/error.tsx
@@ -5,7 +5,7 @@ interface Props {
   error: Error;
 }
 
-// In a real app, you'd probably want to log the error to an error reporting service.
+// In a real app, you'd probably want to log the error to an error reporting service (like "Sentry").
 const ErrorPage = ({ error }: Props) => {
   return <div>An unexpected error occurred.</div>;
 };
